7fce011cdda5b90de83ad20c38e6e6746fbb25be
SunPixel <tarakinnikita@gmail.com>
2024-05-31 09:56:24 +0300

    -+

--- React-app/src/pages/login.jsx
+++ React-app/src/pages/login.jsx
@@ -27,7 +27,9 @@ function window(){
 
 
 function send_data(){
-    
+    var username = document.getElementById('username').value
+    var password = document.getElementById('password').value
+    fetch('http://
 }
 
 
