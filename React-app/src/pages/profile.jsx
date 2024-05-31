5b2c5536ae31f0f47fe2789b7c5796395b8b8448
SunPixel <tarakinnikita@gmail.com>
2024-05-31 08:12:26 +0300

    Profile card added
    not finished yet

--- React-app/src/pages/profile.jsx
+++ React-app/src/pages/profile.jsx
@@ -1,10 +1,24 @@
 import './profile.css'
+import img from '../assets/IMG_1857.jpg'
 
 
 function profile(){
-    <div>
-        Lol
-    </div>
+    return (
+        <div className="Pro_card">
+            <div className='Image'>
+                <img src={img} alt="" style={{ objectFit: 'cover', width: '100%', height: '100%', borderRadius: '50%' }} />
+            </div>
+            <div className='rectangle-16'>
+                <div className='info_Container'>
+                    <div className='rectangle-17'></div>
+                    <div className='rectangle-17'></div>
+                    <div className='rectangle-17'></div>
+                    <div className='rectangle-17'></div>
+                </div>
+            </div>
+            <div className='rect'></div>
+        </div>
+    );
 }
 
 
