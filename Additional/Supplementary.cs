79b6157b86d274ce51c51d43e25455d75ae57882
NickolayMes <144320452+NickolayMes@users.noreply.github.com>
2024-12-12 22:21:25 +0300

    Add files via upload

---
+++ Additional/Supplementary.cs
@@ -0,0 +1,42 @@
+﻿using back.Data;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace back.Additional
+{
+    public class Supplementary
+    {
+        public string MakeHash(string a)
+        {
+            byte[] Data = Encoding.UTF8.GetBytes(a);
+            byte[] HashedData = SHA512.HashData(Data);
+
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < HashedData.Length; i++)
+            {
+                builder.Append(HashedData[i].ToString("x2"));
+            }
+            return builder.ToString();
+        }
+
+        public async Task<string> Id_creator()
+        {
+            var ID = Guid.NewGuid();
+            string id = ID.ToString();
+            var db = new AppDbContext();
+            var existingUser = await db.Users.FirstOrDefaultAsync(u => u.Id == id);
+            if (existingUser != null)
+            {
+                id = await Id_creator();
+            }
+            return id;
+        }
+
+    }
+}
+
+
+
+
---
+++ Additional/UserInitializers.cs
@@ -0,0 +1,52 @@
+﻿using back.Models;
+using Microsoft.Extensions.Logging.Abstractions;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace back.Additional
+{
+    // Intentional so that it doesn't rely on the Model.
+
+    public class UserCredentials
+    {
+        public string Username { get; set; } = null!;
+        public string Password { get; set; } = null!;
+        public int Group_Id { get; set; }
+    }
+
+
+    public class HomeWorkCredentilas
+    {
+        public string Name { get; set; } = null!;       // Наименование предмета которому принадлежит запись
+        public string DeadLine { get; set; } = null!;   // Установка срока выполенния
+        public string? Assignment { get; set; }         // Задание для выполнения
+        public int Group_Id { get; set; }               // Востребованная переменная при создании
+    }
+
+
+    public class ScheduleCredentials
+    {
+        public string Day { get; set; } = null!;
+        public string Time { get; set; } = null!;
+        public string Subject_Name { get; set; } = null!;
+        public int Group_Id { get; set; }
+    }
+
+
+    public class UserInfoCredentials
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string? Email { get; set; }
+        public string? Phone { get; set; }
+        public float? Grade { get; set; }
+        public string University { get; set; } = null!;
+        public string? Speciality { get; set; }
+        public string User_Id { get; set; } = null!;
+    }
+}
+
+
+
+
