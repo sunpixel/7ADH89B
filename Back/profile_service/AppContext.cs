64fe8048048773e8802d47dd84316854c6d8a1f1
SunPixel <tarakinnikita@gmail.com>
2024-12-12 18:21:57 +0300

    Models and AppContext

---
+++ Back/profile_service/AppContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using profile_service.Data_classes;
+
+
+namespace profile_service
+{
+    public class AppDbContext : DbContext
+    {
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Profile> Profiles { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "Data", "Users.db");
+
+            options.UseSqlite(@$"Data Source={path}");
+
+            // Ensure the database is created
+
+            using (var context = new AppDbContext())
+            {
+                context.Database.EnsureCreated();
+            }
+        }
+
+
+    }
+}
---
+++ Back/profile_service/Data_classes/User.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace profile_service.Data_classes
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [NotNull]
+        public string Username { get; set; }
+
+        [Required]
+        [NotNull]
+        public string Password { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [NotNull]
+        public string Email { get; set; }
+
+        public DateTime? CreatedAt { get; set; } = DateTime.UtcNow;
+
+        // Navigation properties
+        public Profile Profile { get; set; }
+    }
+}
