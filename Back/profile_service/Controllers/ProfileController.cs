01d40ce71599a6fa2c7dfe3e20d385dab071f4e4
SunPixel <tarakinnikita@gmail.com>
2024-12-12 21:04:56 +0300

    Logging + Profile editing enhanced

--- Back/profile_service/Controllers/ProfileController.cs
+++ Back/profile_service/Controllers/ProfileController.cs
@@ -27,13 +27,16 @@ namespace profile_service.Controllers
         }
 
 
-        // A test to see if the profile service is running
+        // Retrieve all user profiles
+
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Profile>>> GetProfiles()
         {
             _logger.LogInformation("Getting all profiles");
             return await _context.Profiles.ToListAsync();
         }
+
+        // EDIT: api/Profile/{id}
 
         [HttpPut("{id}")]
         public async Task<IActionResult> EditProfile(int id, Profile profile)
@@ -63,6 +66,58 @@ namespace profile_service.Controllers
 
             return NoContent();
         }
+
+        [HttpPut("/img/{id}")]
+        public async Task<IActionResult> EditProfilePicture(int id, IFormFile profilePicture)
+        {
+            if (profilePicture == null || profilePicture.Length == 0)
+            {
+                return BadRequest();
+            }
+
+            _logger.LogInformation("Editing profile picture for user with id: " + id);
+
+            var profile = await _context.Profiles.FindAsync(id);
+
+            if (profile == null)
+            {
+                _logger.LogInformation("Profile not found");
+                return NotFound();
+            }
+
+            // Converting image to byte array to effectively store in database
+
+            using (var memoryStream = new MemoryStream())
+            {
+                _logger.LogInformation("Converting image to byte array");
+                await profilePicture.CopyToAsync(memoryStream);
+                profile.Profile_picture = memoryStream.ToArray();
+            }
+
+            _context.Entry(profile).State = EntityState.Modified;
+
+            try
+            {
+                _logger.LogInformation("Attempting to save data into the database");
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                _logger.LogInformation("Save attempt faild");
+
+                if (!ProfileExists(id))
+                {
+                    _logger.LogInformation("Profile not found");
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return NoContent();
+        }
+
         public bool ProfileExists(int id)
         {
             return _context.Profiles.Any(e => e.UserId == id);
