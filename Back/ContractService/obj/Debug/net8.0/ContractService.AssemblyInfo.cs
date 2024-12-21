f9faf014620eaf00ca534d83cd3d3f2dd915ac8b
SunPixel <tarakinnikita@gmail.com>
2024-12-22 00:19:36 +0300

    packages updated

--- Back/ContractService/obj/Debug/net8.0/ContractService.AssemblyInfo.cs
+++ Back/ContractService/obj/Debug/net8.0/ContractService.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContractService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce3ba4ad6f87ec0dc9044916176d5dc6763aa672")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd6cc66e00f48e13b3cc6888c02df9b7843b1040")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContractService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContractService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
